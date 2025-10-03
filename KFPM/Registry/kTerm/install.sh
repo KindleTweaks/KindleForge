9ab86f3db49d1e596c251c1e66bdc0d88facae7f
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2025-10-03 18:32:32 +0100

    Attempt kTerm fix

--- KFPM/Registry/kTerm/install.sh
+++ KFPM/Registry/kTerm/install.sh
@@ -1,31 +1,28 @@
 #!/bin/sh
-
 set -e
 
 TMPDIR=/mnt/us/KFPM-Temporary
 mkdir -p "$TMPDIR"
 
-PACKAGE="https://github.com/bfabiszewski/kterm/releases/download/v2.6/kterm-kindle-2.6-armhf.zip"
-
 if [ -f /lib/ld-linux-armhf.so.3 ]; then
-  # Hard Float
+  PACKAGE="https://github.com/bfabiszewski/kterm/releases/download/v2.6/kterm-kindle-2.6-armhf.zip"
 else
-  # Soft Float
   PACKAGE="https://github.com/bfabiszewski/kterm/releases/download/v2.6/kterm-kindle-2.6.zip"
 fi
 
 # Download + Extract
-curl -L -o "$TMPDIR/kterm.zip" $PACKAGE
+curl -L -o "$TMPDIR/kterm.zip" "$PACKAGE"
 unzip -q "$TMPDIR/kterm.zip" -d "$TMPDIR"
 
-# First Subfolder
+# Subdir Or TMPDIR
 SUBDIR=$(find "$TMPDIR" -mindepth 1 -maxdepth 1 -type d | head -n 1)
+[ -z "$SUBDIR" ] && SUBDIR="$TMPDIR"
 
 # Copy Contents
 mkdir -p /mnt/us/extensions/kterm
-cp -r "$SUBDIR"/* /mnt/us/extensions/kterm
+cp -r "$SUBDIR"/. /mnt/us/extensions/kterm/
 
 # Cleanup
-rm -rf $TMPDIR
+rm -rf "$TMPDIR"
 
 exit 0
