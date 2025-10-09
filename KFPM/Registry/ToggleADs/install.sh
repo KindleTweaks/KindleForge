a0572cc0f8d3408b29af1d3e5d39d863ff3233e6
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2025-10-09 16:53:15 +0100

    Attempt Finish ToggleADs

---
+++ KFPM/Registry/ToggleADs/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+curl -L -o /mnt/us/documents/toggle-ads.sh https://raw.githubusercontent.com/polish-penguin-dev/KindleForge/refs/heads/master/KFPM/Registry/ToggleADs/assets/toggle-ads.sh
+
+exit 0
