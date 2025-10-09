6d21b35d73589fb6f56c3ba2cd10bab4d76f4f6c
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2025-10-09 17:28:01 +0100

    Update ToggleADs

--- KFPM/Registry/ToggleAds/install.sh
+++ KFPM/Registry/ToggleAds/install.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-curl -L -o /mnt/us/documents/toggle-ads.sh https://raw.githubusercontent.com/polish-penguin-dev/KindleForge/refs/heads/master/KFPM/Registry/ToggleADs/assets/toggle-ads.sh
+curl -L -o /mnt/us/documents/toggle-ads.sh https://raw.githubusercontent.com/polish-penguin-dev/KindleForge/refs/heads/master/KFPM/Registry/ToggleAds/assets/toggle-ads.sh
 
 exit 0
